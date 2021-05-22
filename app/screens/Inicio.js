1af33c17c538418467693fccab29123323eb4a22
LeonardoMartinezGonzalez <msc_lmartinez@accitesz.com>
2021-05-22 10:12:21 -0500

    Mi primer cambio

--- app/screens/Inicio.js
+++ app/screens/Inicio.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Articulo() {
     return (
         <View>
-            <Text>INICIO</Text>
+            <Text>INICIO con cambio</Text>
         </View>
     );
 }
